9374d2df49680ca1aa8593c2d028e1a41d35e0bf
tgb2k24 <tgb2k24@gmail.com>
2026-02-22 16:08:32 +0530

    fix: Rename dist directories to build-main and build-admin to avoid Windows space artifacts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   distDir: (process.env.DIST_DIR || '.next').trim(),
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   reactCompiler: true,
 };
+console.log('Using distDir: ["' + nextConfig.distDir + '"] Length:', nextConfig.distDir?.length);
 
 export default nextConfig;

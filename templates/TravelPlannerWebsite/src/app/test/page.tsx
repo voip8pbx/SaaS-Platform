8f48e31fb9bb45712bf20135946aca0de5eb693a
tgb2k24 <tgb2k24@gmail.com>
2026-02-27 01:07:11 +0530

    Add routing test page to template

---
+++ templates/TravelPlannerWebsite/src/app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: '100px', textAlign: 'center' }}>
+            <h1>Routing Test Successful</h1>
+            <p>If you see this, Next.js routing is working in the template.</p>
+        </div>
+    );
+}

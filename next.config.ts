c537769bfac4c0b0b9a664bf8fa3e59f4620d2e5
tgb2k24 <tgb2k24@gmail.com>
2026-02-28 21:12:21 +0530

    sgg

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: (process.env.VERCEL_PROJECT_NAME === 'saas-platform-superadmin')
-    ? 'build-main'
-    : (process.env.VERCEL_PROJECT_NAME === 'saas-platform-admindashboard')
-      ? 'build-admin'
-      : (process.env.DIST_DIR || '.next'),
+  distDir: process.env.DIST_DIR || '.next',
   logging: {
     fetches: {
       fullUrl: true,

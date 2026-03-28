a5a64e86a8080776725799c0e8fb80d1ffd8530c
voip8pbx <voip8pbx@gmail.com>
2026-03-28 20:27:19 +0530

    Regualr push

--- build-main/dev/server/app/api/superadmin/assign/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/superadmin/assign/route.js")
-R.c("server/chunks/[root-of-the-server]__b7a5da75._.js")
-R.c("server/chunks/[root-of-the-server]__897563e9._.js")
-R.c("server/chunks/_next-internal_server_app_api_superadmin_assign_route_actions_724b470a.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/superadmin/assign/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/src/app/api/superadmin/assign/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- templates/TravelPlannerWebsite/src/config/siteConfig.ts
+++ templates/TravelPlannerWebsite/src/config/siteConfig.ts
@@ -1,21 +1,21 @@
 export const siteConfig = {
-    name: "sherrrr",
-    adminUserId: "admin456",
-    description: "yoyoyoyoyyoyo",
+    name: "Sky Trip",
+    adminUserId: "Admin123",
+    description: "The best way to plan your next adventure.",
     logo: "/uploads/logo-1769412461002.png",
     primaryColor: "#8b5cf6",
     customColors: {
         background: "",
         header: ""
     },
-    theme: "dark",
+    theme: "system",
     layout: "modern",
     heroImage: "https://images.unsplash.com/photo-1436491865332-7a61a109cc05?q=80&w=2074&auto=format&fit=crop",
     features: {
         flights: false,
         packages: true,
         hotels: true,
-        trains: true,
+        trains: false,
         cabs: true,
         rentals: true,
         cars: false,
@@ -25,9 +25,9 @@ export const siteConfig = {
         news: true
     },
     contact: {
-        phone: "88888888",
+        phone: "123456",
         email: "lalalala@gmsil.com",
-        address: "55fgvctxcchv"
+        address: "aaaaaaaaaaaa"
     },
     social: {
         twitter: "https://twitter.com/travelpanner",
